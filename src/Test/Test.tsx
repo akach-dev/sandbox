86d62786e15286b3abf55ad1f7040c389841fd47
Alexander Kachan <akach.dev@mail.ru>
2023-07-05 19:12:44 +0300

    updata test component

--- src/Test/Test.tsx
+++ src/Test/Test.tsx
@@ -1,7 +1,12 @@
 export const Test = () => {
   return (
-      <div>
-        123
-      </div>
+      <>
+        <div>
+          123
+        </div>
+        <div>
+          Dev branch commit 1
+        </div>
+      </>
   )
 }
