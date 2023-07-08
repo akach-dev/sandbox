f3704269053429a92a7cb6c10e953028b6461fe9
Alexander Kachan <akach.dev@mail.ru>
2023-07-08 19:07:30 +0300

    updeate 19:07

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,13 @@
 import React from 'react';
 import './App.css';
+import {User} from "./User";
 
-export function User() {
-  return (
-      <div>
-        <h3>What to learn</h3>
-        <div>
-          <input/>
-          <button>+</button>
-        </div>
-        <ul>
-          <li><input checked={true} type="checkbox"/>CSS & HTML</li>
-          <li><input checked={true} type="checkbox"/>JS</li>
-          <li><input checked={false} type="checkbox"/>REACT</li>
-          <div>
-            <button>All</button>
-            <button>Active</button>
-            <button>Completed</button>
-          </div>
-        </ul>
-      </div>
-  )
-}
-
-function App() {
+export function App() {
   return (
       <div className="App">
+        <User/>
+        <User/>
         <User/>
       </div>
   );
 }
-
-export default App
---
+++ src/User.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export function User() {
+  return (
+      <div>
+        <h3>What to learn</h3>
+        <div>
+          <input/>
+          <button>+</button>
+        </div>
+        <ul>
+          <li><input checked={true} type="checkbox"/>CSS & HTML</li>
+          <li><input checked={true} type="checkbox"/>JS</li>
+          <li><input checked={false} type="checkbox"/>REACT</li>
+          <div>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+          </div>
+        </ul>
+      </div>
+  )
+}
